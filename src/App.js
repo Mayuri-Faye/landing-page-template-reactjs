408b18835e4c9964dc2ed4fe19ff141303249f3a
Mayuri-Faye <mayu.wandhare@gmail.com>
2026-02-06 15:07:24 +0530

    Add comment for Mayuri's changes
    Added a comment indicating changes made by Mayuri.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 
+// this is mayuri changes
+
 function App() {
   return (
     <>
